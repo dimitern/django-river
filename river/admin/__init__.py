9172496d66c7e614f0d958e10897d40a2f0d433c
Dimiter Naydenov <dimiter@naydenov.net>
2018-03-16 10:47:37 +0200

    Admin improvements

--- river/admin/__init__.py
+++ river/admin/__init__.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
-
 from river.admin.proceeding_meta import ProceedingMetaAdmin
-from river.models import State, Transition
+from river.models import State
+from river.models import Transition
 
-admin.site.register(State)
-admin.site.register(Transition)
+
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+
+    list_display = ('label',
+                    'slug',
+                    'description')
+
+    ordering = ('label',)
+
+
+@admin.register(Transition)
+class TransitionAdmin(admin.ModelAdmin):
+
+    list_display = ('__str__',
+                    'source_state',
+                    'destination_state',
+                    'direction')
+
+    ordering = ('source_state',)
--- river/admin/proceeding_meta.py
+++ river/admin/proceeding_meta.py
@@ -1,7 +1,9 @@
 from django import forms
 from django.contrib import admin
 from django.contrib.contenttypes.models import ContentType
-
+from django.utils.html import format_html
+from django.utils.html import format_html_join
+from django.utils.translation import ugettext_lazy as _
 from river.models import ProceedingMeta
 from river.models.fields.state import StateField
 from river.services.object import ObjectService
@@ -26,7 +28,8 @@ class ProceedingMetaForm(forms.ModelForm):
         fields = ('content_type', 'transition', 'permissions', 'groups', 'order', 'action_text')
 
     def __init__(self, *args, **kwargs):
-        self.declared_fields['content_type'].queryset = ContentType.objects.filter(pk__in=get_content_types())
+        self.declared_fields['content_type'].queryset = ContentType.objects.filter(
+            pk__in=get_content_types())
 
         super(ProceedingMetaForm, self).__init__(*args, **kwargs)
 
@@ -40,6 +43,37 @@ class ProceedingMetaForm(forms.ModelForm):
 
 class ProceedingMetaAdmin(admin.ModelAdmin):
     form = ProceedingMetaForm
+
+    def permissions_list(self, obj):
+        if obj.permissions.count() > 0:
+            return format_html(
+                '<ul>{}</ul>',
+                format_html_join(
+                    '\n', '<li>{}</li>',
+                    obj.permissions.values_list('name')))
+
+    permissions_list.short_description = _('Permissions')
+    permissions_list.admin_order_field = 'permissions'
+
+    def groups_list(self, obj):
+        if obj.groups.count() > 0:
+            return format_html(
+                '<ul>{}</ul>',
+                format_html_join(
+                    '\n', '<li>{}</li>',
+                    obj.groups.values_list('name')))
+
+    groups_list.short_description = _('Groups')
+    groups_list.admin_order_field = 'groups'
+
+    list_display = ('content_type',
+                    'transition',
+                    'permissions_list',
+                    'groups_list',
+                    'order',
+                    'action_text')
+
+    ordering = ('content_type', 'transition', 'order')
 
 
 admin.site.register(ProceedingMeta, ProceedingMetaAdmin)
